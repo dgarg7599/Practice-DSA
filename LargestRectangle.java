afaca954f8072b35137ac2491e04822189131414
dgarg7599 <gargdivyansh7599@gmail.com>
2025-05-08 13:33:18 +0530

    [Divyansh Garg] ADD: Done Largest Rectangle.

---
+++ LargestRectangle.java
@@ -0,0 +1,75 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'largestRectangle' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts INTEGER_ARRAY h as parameter.
+     */
+
+    public static long largestRectangle(List<Integer> h) {
+        // Write your code here
+        int n = h.size();
+        int[] left = new int[n];
+        int[] right = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && h.get(stack.peek()) >= h.get(i)) {
+                stack.pop();
+            }
+            left[i] = stack.isEmpty() ? -1 : stack.peek();
+            stack.push(i);
+        }
+
+        stack.clear();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && h.get(stack.peek()) >= h.get(i)) {
+                stack.pop();
+            }
+            right[i] = stack.isEmpty() ? n : stack.peek();
+            stack.push(i);
+        }
+        long maxArea = 0;
+        for (int i = 0; i < n; i++) {
+            long area = (long) h.get(i) * (right[i] - left[i] - 1);
+            maxArea = Math.max(maxArea, area);
+        }
+
+        return maxArea;
+
+    }
+
+}
+
+public class LargestRectangle {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> h = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        long result = Result.largestRectangle(h);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
