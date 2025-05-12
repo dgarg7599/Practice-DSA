1d4dbf3b5e80bacdf837165f7083456fd80ecaf6
dgarg7599 <gargdivyansh7599@gmail.com>
2025-05-12 17:43:06 +0530

    [Divyansh Garg] ADD: Done Design Browser History.

---
+++ BrowserHistory.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class BrowserHistory {
+
+    List<String> history;
+    int currentIndex;
+
+    public BrowserHistory(String homepage) {
+        history = new ArrayList<>();
+        history.add(homepage);
+        currentIndex = 0;
+    }
+
+    public void visit(String url) {
+        while (history.size() > currentIndex + 1) {
+            history.remove(history.size() - 1);
+        }
+        history.add(url);
+        currentIndex++;
+    }
+
+    public String back(int steps) {
+        currentIndex = Math.max(0, currentIndex - steps);
+        return history.get(currentIndex);
+    }
+
+    public String forward(int steps) {
+        currentIndex = Math.min(history.size() - 1, currentIndex + steps);
+        return history.get(currentIndex);
+    }
+}
+
+
+/**
+ * Your BrowserHistory object will be instantiated and called as such:
+ * BrowserHistory obj = new BrowserHistory(homepage);
+ * obj.visit(url);
+ * String param_2 = obj.back(steps);
+ * String param_3 = obj.forward(steps);
+ */
