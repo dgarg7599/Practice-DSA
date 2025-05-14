b67ad9c4460ebabc870ec322302070ae13418e0c
dgarg7599 <gargdivyansh7599@gmail.com>
2025-05-14 09:36:44 +0530

    [Divyansh Garg] ADD: Done PeakIndex in a Mountain Array.

--- PeakIndex.java
+++ PeakIndex.java
@@ -1,17 +1,33 @@
-class Solution {
-    public int PeakIndex(int[] arr) {
-        int low = 0;
-        int high = arr.length - 1;
+class PeakIndex {
+    public int peakIndexInMountainArray(int[] arr) {
 
-        while (low < high) {
-            int mid = low + (high - low) / 2;
-            if (arr[mid] < arr[mid + 1]) {
-                low = mid + 1;
-            } else {
-                high = mid;
-            }
+        // int low = 0;
+        // int high = arr.length - 1;
+
+        // while (low < high) {
+        //     int mid = low + (high - low) / 2;
+        //     if (arr[mid] < arr[mid + 1]) {
+        //         low = mid + 1;
+        //     } else {
+        //         high = mid;
+        //     }
+        // }
+
+        // return low;
+
+        return binarySearchPeak(arr, 0, arr.length - 1);
+
+    }
+
+    private int binarySearchPeak(int[] arr, int low, int high) {
+        if (low == high) {
+            return low;
         }
-
-        return low;
+        int mid = low + (high - low) / 2;
+        if (arr[mid] < arr[mid + 1]) {
+            return binarySearchPeak(arr, mid + 1, high);
+        } else {
+            return binarySearchPeak(arr, low, mid);
+        }
     }
 }
